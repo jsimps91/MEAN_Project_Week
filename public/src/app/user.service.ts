fd62563c9085409e2581aeaf05c3cc4fbcef76ef
Alyssa Nickow <alyssa@nickow.com>
2017-08-23 11:24:09 -0500

    committingMerge branch 'master' of https://github.com/ANickow/MEAN_Project_Week

--- public/src/app/user.service.ts
+++ public/src/app/user.service.ts
@@ -9,27 +9,23 @@ export class UserService {
   constructor(private _http: Http) { }
 
   regAttempt(user) {
-    console.log('MADE IT REGATTEMPT IN SERVICE');
     return this._http.post('/api/reg', user)
     .map(data => data.json())
     .toPromise();
   }
 
   loginAttempt(user) {
-    console.log('GOT TO THE LOGIN SERVICE');
     return this._http.post('/api/login', user)
     .map(data => data.json())
     .toPromise();
   }
 
   getCurrentUser(){
-    console.log("GETTING CURRENT USER")
     return this._http.get('/api/show_user',)
     .map(data => data.json()).toPromise();   
   }
 
   showProfile(id){
-    console.log("SHOW USER PROFILE GOING INTO HTTP REQUEST", id)
     return this._http.get(`api/show_profile/${id}`)
     .map(data => data.json())
     .toPromise();
@@ -42,14 +38,5 @@ export class UserService {
     .toPromise();
   }
 
-  // getUser() {
-  //   return this.currentUser;
-  // }
-
-  // cancel(appoint) {
-  //   return this._http.post('/api/cancel', appoint)
-  //   .map(data => data.json())
-  //   .toPromise();
-  // }
 
 }
