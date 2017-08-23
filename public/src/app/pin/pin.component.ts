3f79724ed2b861e44e2f46e8525348fc84d3ca6a
Jessica <jessicaesimpson91@gmail.com>
2017-08-23 11:37:20 -0500

    fixed input error, made pin an input property of pin component so page would load

--- public/src/app/pin/pin.component.ts
+++ public/src/app/pin/pin.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Pin } from '../pin';
 @Component({
   selector: 'app-pin',
   templateUrl: './pin.component.html',
   styleUrls: ['./pin.component.css']
 })
 export class PinComponent implements OnInit {
+
+  @Input() pin;
+
 
   constructor() { }
 
