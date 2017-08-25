d131af89aa6db6c85090ae6519501c9be1241ca6
Alyssa Nickow <alyssa@nickow.com>
2017-08-24 19:48:55 -0500

    updated the Angular user class to match the API user model

--- public/src/app/user.ts
+++ public/src/app/user.ts
@@ -4,6 +4,10 @@ export class User {
         public email: string = '',
         public age: number = 0,
         public gender: string = '',
-        public password: string = ''
+        public password: string = '',
+        public boards = [],
+        public pins = [],
+        public followers = [],
+        public following = []
     ) {}
 }
